d4daee2d6f5d2bf8a89c84aa65b5ef7a168a6e15
unknown <adstep@github.com>
2018-04-15 00:19:20 -0700

    working preview html with video and audio

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,51 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+import { TextDocumentContentProvider, EventEmitter, Uri, Event, CancellationToken } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+export class TestContentProvider implements TextDocumentContentProvider {
+    private _onDidChange = new EventEmitter<Uri>();
+
+    get onDidChange(): Event<Uri> {
+        return this._onDidChange.event;
+    }
+
+    update(uri: Uri) {
+        this._onDidChange.fire(uri);
+    }
+
+    provideTextDocumentContent(uri: Uri, token: CancellationToken): string | Thenable<string> {
+        return `
+        <html>
+            <body>
+                <audio controls autoplay src="http://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.oga"></audio>
+                <video controls autoplay>
+                    <source src="http://media.w3.org/2010/05/sintel/trailer.mp4"
+                        type='video/mp4; codecs="avc1, mp4a"'>
+                    <source src="http://media.w3.org/2010/05/sintel/trailer.ogv"
+                        type='video/ogg; codecs="theora, vorbis"'>
+                </video>
+            </body>
+        </html>
+        `;
+    }
+}
+
 export function activate(context: vscode.ExtensionContext) {
+    const testUri = vscode.Uri.parse('vscode-video-audio://authority/vscode-video-audio');
+    const provider = new TestContentProvider();
+    const registration = vscode.workspace.registerTextDocumentContentProvider('vscode-video-audio', provider);
+    provider.update(testUri);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-video-audo" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    const disposable = vscode.commands.registerCommand('extension.testPreviewHtml', () => {
+        vscode.commands.executeCommand('vscode.previewHtml', testUri, vscode.ViewColumn.One).then(
+            (success) => { },
+            (reason) => { vscode.window.showErrorMessage(reason)}
+        );
     });
 
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
